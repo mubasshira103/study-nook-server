9c3b9a706baa6fff735ffe70527e755b9e38d22b
mubasshira103 <khadijamubasshira103@gmail.com>
2026-05-20 13:17:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
